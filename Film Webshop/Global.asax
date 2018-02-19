c8b20ff5490a2331fbb8a8de9fdd99371628f1c8
mepbat <s.westerburger@student.fontys.nl>
2018-02-19 11:35:59 +0100

    Add project files.

---
+++ Film Webshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Film_Webshop.MvcApplication" Language="C#" %>
---
+++ Film Webshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Film_Webshop.Startup))]
+namespace Film_Webshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
