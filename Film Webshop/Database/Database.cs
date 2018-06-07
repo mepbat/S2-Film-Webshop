2443bb72d1bf06ea210d2d34165de2d5e534b584
Simon Westerburger <simon.westerburger@gmail.com>
2018-06-07 14:20:00 +0200

    inlog db

--- Film Webshop/Database/Database.cs
+++ Film Webshop/Database/Database.cs
@@ -14,7 +14,7 @@ namespace Film_Webshop.Database
             //local
             //ConnectionString = "Data Source=mssql.fhict.local;Initial Catalog = dbi383656; Persist Security Info=True;User ID = dbi383656; Password=Wachtwoord123";
             //azure
-            ConnectionString = "Server=tcp:filmwebshop.database.windows.net,1433;Initial Catalog=FilmWebshop;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            ConnectionString = "Server=tcp:filmwebshop.database.windows.net,1433;Initial Catalog=FilmWebshop;Persist Security Info=False;User ID=simon4224;Password=Wachtwoord4224;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         }
     }
 }
